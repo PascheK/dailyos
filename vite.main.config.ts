fe844c0dffeb23c69902cf4ac9649cb4aa86e787
PascheK <killian.pasche7@gmail.com>
2026-05-15 13:28:57 +0200

    fix: externalize better-sqlite3 from Vite bundle

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -5,5 +5,15 @@ export default defineConfig({
   resolve: {
     conditions: ['node'],
     mainFields: ['module', 'jsnext:main', 'jsnext']
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'better-sqlite3',
+        'bindings',
+        'file-uri-to-path',
+        'node-gyp-build'
+      ]
+    }
   }
 })
